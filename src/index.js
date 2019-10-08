b3eaa1b63909b31c8f516960359e853e830185b4
Anastasiia Sierbaieva <serbayeva@gmail.com>
2019-10-08 15:21:57 -0700

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	let configMap = new Map();
+        
+        for (element of bracketsConfig) {
+            configMap.set(element[0], element[1])
+        }
+
+        let stack = [];
+
+        for (let i = 0; i < str.length; i++) {
+            let strVal = str[i];
+
+            if (configMap.has(strVal) &&
+                configMap.get(strVal) == strVal &&
+                stack[stack.length - 1] == strVal) {
+                stack.pop();
+            } else if(configMap.has(strVal)) {
+                stack.push(strVal);
+            } else {
+                let prev = stack.pop();
+                if (strVal !== configMap.get(prev)) {
+                    return false;
+                }
+            }
+        }
+        if (stack.length > 0) return false;
+        return true;
 }
